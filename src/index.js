47d3b762ddbcc661c8108db922ee830882bde70e
praneeth gurram <gpran93@vt.edu>
2020-06-01 08:52:52 -0400

    Add Component structure to the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
+import BusinessList from "./components/BusinessList/BusinessList";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <BusinessList />
   </React.StrictMode>,
   document.getElementById('root')
 );
