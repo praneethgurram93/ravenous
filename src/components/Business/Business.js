32e8706a703b41e7bdb6ad8c216a06dac23b8830
praneeth gurram <gpran93@vt.edu>
2020-06-01 18:34:59 -0400

    fix alignment issues in Business.js

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -23,16 +23,14 @@ const Business = () => {
       <h2>{business.name}</h2>
       <div className="Business-information">
         <div className="Business-address">
-          <div className="Business-address">
-            <p>{business.address}</p>
-            <p>{business.city}</p>
-            <p>`${business.state}  ${business.zipCode}`</p>
-          </div>
-          <div className="Business-reviews">
-            <h3>{business.category}</h3>
-            <h3 className="rating">{business.rating}</h3>
-            <p>`${business.reviewCount}  reviews`</p>
-          </div>
+          <p>{business.address}</p>
+          <p>{business.city}</p>
+          <p>{`${business.state}  ${business.zipCode}`}</p>
+        </div>
+        <div className="Business-reviews">
+          <h3>{business.category}</h3>
+          <h3 className="rating">{business.rating}</h3>
+          <p>{`${business.reviewCount}  reviews`}</p>
         </div>
       </div>
     </div>
