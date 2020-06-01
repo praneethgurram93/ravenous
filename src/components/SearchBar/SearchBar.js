8dc25d562a370d740d66b0c6aa3ed0c9b9155a99
praneeth gurram <gpran93@vt.edu>
2020-06-01 18:29:45 -0400

    Remove default create react app text

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,6 +3,35 @@ import './SearchBar.css';
 
 const SearchBar = () => {
 
+  const sortByOptions = {
+    "Best Match": "best_match",
+    "Highest Rated": "rating",
+    "Most Reviewed": "review_count"
+  };
+
+  const renderSortByOptions = () => (
+    Object.keys(sortByOptions).map(sortByOption => (
+      <li key={sortByOptions[sortByOption]}>{sortByOption}</li>
+    ))
+  );
+
+  return (
+    <div className="searchBar">
+      <div className="SearchBar-sort-options">
+        <ul>
+          {renderSortByOptions()}
+        </ul>
+      </div>
+      <div className="SearchBar-fields">
+        <input placeholder="Search Businesses" />
+        <input placeholder="Where?"/>
+      </div>
+      <div className="SearchBar-submit">
+        <a>Let's Go</a>
+      </div>
+    </div>
+  );
+
 };
 
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import BusinessList from "./components/BusinessList/BusinessList";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/*<App />*/}
-    <BusinessList />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
