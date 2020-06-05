5d0c2a58c3f10b06d668195c838333d9619ab651
praneeth gurram <gpran93@vt.edu>
2020-06-05 18:48:45 -0400

    Add input handler functions in SearchBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SearchBar.css';
 
 const SearchBar = () => {
+  const [term, setTerm] = useState('');
+  const [location, setLocation] = useState('');
+  const [sortBy, setSortBy] = useState('best_match');
 
   const sortByOptions = {
     "Best Match": "best_match",
@@ -9,11 +12,36 @@ const SearchBar = () => {
     "Most Reviewed": "review_count"
   };
 
+  const getSortByClass = sortByOption => {
+    return sortBy === sortByOption ? 'active' : '';
+  };
+
+  const handleTermChange = event => setTerm(event.target.value);
+
+  const handleLocationChange = event => setLocation(event.target.value);
+
+  const searchYelp = () => {
+    console.log('Searching Yelp with Pizza, Brooklyn, best_match');
+  };
+
+  const handleSearch = event => {
+    searchYelp();
+    event.preventDefault();
+  };
+
   const renderSortByOptions = () => (
     Object.keys(sortByOptions).map(sortByOption => (
-      <li key={sortByOptions[sortByOption]}>{sortByOption}</li>
+      <li
+        className={getSortByClass(sortByOptions[sortByOption])}
+        value={sortByOption}
+        onClick={() => setSortBy(sortByOptions[sortByOption])}
+        key={sortByOptions[sortByOption]}
+      >
+        {sortByOption}
+      </li>
     ))
   );
+
 
   return (
     <div className="SearchBar">
@@ -23,11 +51,19 @@ const SearchBar = () => {
         </ul>
       </div>
       <div className="SearchBar-fields">
-        <input placeholder="Search Businesses" />
-        <input placeholder="Where?"/>
+        <input
+          placeholder="Search Businesses"
+          value={term}
+          onChange={handleTermChange}
+        />
+        <input
+          placeholder="Where?"
+          value={location}
+          onChange={handleLocationChange}
+        />
       </div>
       <div className="SearchBar-submit">
-        <a>Let's Go</a>
+        <a onClick={handleSearch}>Let's Go</a>
       </div>
     </div>
   );
